5a64a786cf6ac354826c6bd8712a48e07cc8f693
magic frog <estella.tianxing@gmail.com>
2024-07-04 14:15:24 -0400

    update

--- pages/projects/the-trail-meshenger.js
+++ pages/projects/the-trail-meshenger.js
@@ -3,12 +3,11 @@ import Image from "next/image";
 const TheTrailMeshenger = () => {
   return (
     <div>
-      <div className="trail-image"> <Image 
+      <div className="project-main-image"> <Image 
         src="/images/trailmeshenger2.png" 
         alt="Trail Board Image" 
         width={550} 
-        height={400} 
-      />
+        height={400}         />
       </div>
       <h1>The Trail Meshenger</h1>
       <p>created by Evan Gan, Cosmin Mare, Jirayu A,Elliot Shumsky, & Krishna Kesavalu</p>
