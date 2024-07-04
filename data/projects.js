9eb14711da86ecabad96b504e7be2c773d6af47c
magic frog <estella.tianxing@gmail.com>
2024-07-04 19:44:46 -0400

    update

--- data/projects.js
+++ data/projects.js
@@ -1,20 +1,26 @@
 export const projects = [
-    {
-      id: 1,
-      name: 'The Trail Meshenger',
-      description: '"one stop shop for long range communications in the middle of nowhere!" The Trail Meshenger has an antenna, screen, raspberry pi and it uses LoRa meshing to transmit and receive messages!',
-      image: '/public/images/thetrailmeshenger1.jpg'
-    },
-    {
-      id: 2,
-      name: 'The TRAILDEV Computer',
-      description: 'Description for Project 2',
-      image: '/public/images/project2.jpg'
-    },
-    {
-      id: 3,
-      name: 'Project 3',
-      description: 'Description for Project 3',
-      image: '/public/images/project3.jpg'
-    }
-  ];
+  {
+    id: 1,
+    name: 'The Trail Meshenger',
+    author: 'Evan Gan, Cosmin Mare, Jirayu A,Elliot Shumsky, & Krishna Kesavalu',
+    description: [
+      '"one stop shop for long range communications in the middle of nowhere!"',
+     'The Trail Meshenger is a messenger with an antenna, screen, raspberry pi that uses LoRa meshing to trasmit and receive message. During the trail, there will be no internet, so no communication. Luckily, with our board you can send messages to your peers using a full keyboard, receive messages from afar that have been bounced to you via LoRa meshing, and scroll through history to view all your messages.'
+    ],
+    image: '/images/trailmeshenger2.png'
+  },
+  {
+    id: 2,
+    name: 'The TRAILDEV Computer',
+    author: 'someone',
+    description: 'Description for Project 2',
+    image: '/images/project2.jpg'
+  },
+  {
+    id: 3,
+    name: 'Project 3',
+    author: 'someone else',
+    description: 'Description for Project 3',
+    image: '/images/project3.jpg'
+  }
+];
---
+++ pages/projects/[slug].js
@@ -0,0 +1,77 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { projects } from '../../data/projects';
+import styles from '@/styles/Project.module.css';
+
+function generateDescriptionHTML(descriptionParts) {
+    return descriptionParts.map(part => `<p>${part}</p>`).join('');
+  }
+
+export default function ProjectPage({ project }) {
+  const router = useRouter();
+  const { slug } = router.query;
+  if (!router.isFallback && !project?.name) {
+    return <div>404 - Project Not Found</div>;
+  }
+
+  const description = generateDescriptionHTML(project.description);
+
+  return (
+    <div className="project-page">
+      <div className={styles.projectImage}>
+        <Image
+          src={project.image}
+          alt={project.name}
+          width={400}
+          height={250}
+          layout="responsive"
+          objectFit="cover"
+          objectPosition="center"
+        />
+        </div>
+      <Head>
+        <title>{project.name} - THE TRAIL BOARD</title>
+      </Head>
+      <h1>{project.name}</h1>
+      <p>created by {project.author}</p>
+      <div dangerouslySetInnerHTML={{ __html: description }} />
+      
+    {project.video && (
+        <div className={styles.projectVideo}>
+          <iframe
+            width="560"
+            height="315"
+            src={project.video}
+            title={project.name}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+    )}
+     <Link href="/" passHref>
+        <button className="back-button">Back to Home</button>
+      </Link>
+    </div>
+  );
+}
+export async function getStaticPaths() {
+  const paths = projects.map(project => ({
+    params: { slug: project.name.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '') },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const project = projects.find(proj => proj.name.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '') === slug);
+  return {
+    props: {
+      project,
+    },
+  };
+}
